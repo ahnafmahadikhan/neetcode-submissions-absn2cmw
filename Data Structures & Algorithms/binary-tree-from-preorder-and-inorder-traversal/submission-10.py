2499d4f8853cb88e39b24cc5267a5c17ddce232c
ahnafmahadikhan <ahnafmahadikhan999@gmail.com>
2026-09-05 21:32:43 +0600

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-10

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-10.py
@@ -0,0 +1,27 @@
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+
+        pos = {}
+
+        for i in range(len(inorder)):
+            pos[inorder[i]] = i
+
+        preIndex = 0
+
+        def dfs(left, right):
+            nonlocal preIndex
+
+            if left > right:
+                return None
+
+            root = TreeNode(preorder[preIndex])
+            preIndex += 1
+
+            mid = pos[root.val]
+
+            root.left = dfs(left, mid - 1)
+            root.right = dfs(mid + 1, right)
+
+            return root
+
+        return dfs(0, len(inorder) - 1)
